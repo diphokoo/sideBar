1122c23f6398800fc4b05534886e17dc518a6cf3
Obakeng Freddy Diphoko <42438019+diphokoo@users.noreply.github.com>
2024-01-24 10:28:40 +0200

    file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SideBar from './Component/Sidebar';
+import DashbaordI from './Pages/Dashboard';
+import About from './Pages/About';
+import Comment from './Pages/Comment';
+import Analytics from './Pages/Analytics';
+import Product from './Pages/Product';
+import ProductList from './Pages/ProductList';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <SideBar>
+      <Routes>
+        <Route path="/" element={<DashbaordI />} />
+        <Route path="/DashbaordI" element={<DashbaordI />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Comment" element={<Comment />} />
+        <Route path="/Analytics" element={<Analytics />} />
+        <Route path='/Product' element={<Product />} />
+        <Route path='/ProductList' element={<ProductList />} />
+      </Routes>
+    </SideBar>
+  </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/Component/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+    FaTh,
+    FaBars,
+    FaUserAlt,
+    FaRegChartBar,
+    FaCommentAlt,
+    FaShoppingBag,
+    FaThList
+}from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+
+function SideBar() {
+    return (
+        <div>
+            <h1>
+                SideBar
+            </h1>
+        </div>
+    )
+}
+
+export default SideBar;
